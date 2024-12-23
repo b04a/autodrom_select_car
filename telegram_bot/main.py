85ec01e3d452c5c25faeddd73e1c53912c5efb5f
+futurehendrix <bochka13@list.ru>
2024-12-23 18:45:02 +0400

    update method work bot tg

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,47 +1,81 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, WebAppInfo
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
 
 # Токен вашего бота
-API_TOKEN = 'XXX'
+API_TOKEN = '8005517380:AAGxa2KjbwDrM0ndrGYIDB0TTh6dLzFN-SY'
 
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(storage=MemoryStorage())
+
+
+# Определение состояний
+class SearchCarState(StatesGroup):
+    waiting_for_price_range = State()
+
+
+# Клавиатура с кнопками
+main_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🚗 Найти машину")],
+    ],
+    resize_keyboard=True
+)
+
 
 # Хэндлер команды /start
 @dp.message(Command("start"))
-async def send_welcome(message: Message):
-    await message.answer("""
-        Привет! Почекаем тачки?
-        /help - Узнать о способности бота
-    """)
+async def start_command(message: Message):
+    await bot.send_photo(
+        chat_id=message.chat.id,
+        photo="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/b2b63a69331555.5b7d61c8a04d5.jpg",
+        # Ссылка на изображение или файл
+        caption="Привет! Я бот для поиска машин на основе сервиса drom.ru! Открывай меню!",
+        reply_markup=main_keyboard
+    )
 
-@dp.message(Command("help"))
-async def help(message: Message):
-    await message.answer("/help - Узнать о способности бота\n"
-                         "/search_car - Поиск машины по запросу")
 
-@dp.message(Command("search_car"))
-async def search_car(message: Message):
+# Хэндлер кнопки "🚗 Найти машину"
+@dp.message(F.text == "🚗 Найти машину")
+async def find_car_command(message: Message, state: FSMContext):
     await message.answer("""
-        Давай я подберу тебе машину!
-        В какой цене искать?
-        
-        1 число - минимальная цена
-        2 число - максимальная цена 
-        Напиши в формате:
-        (100000 300000)
-        
-        Где 1 число 
+        В каком ценовом диапозоне ищем?
+        Напиши минимальную цену и максимальную
+        В формате: 100000 200000
     """)
+    # Устанавливаем состояние ожидания диапазона цен
+    await state.set_state(SearchCarState.waiting_for_price_range)
+
+
+# Обработка ввода диапазона цен
+@dp.message(SearchCarState.waiting_for_price_range)
+async def process_price_range(message: Message, state: FSMContext):
+    try:
+        # Парсим введенные данные
+        min_price, max_price = map(int, message.text.split())
+
+        # Вывод в чат
+        await message.answer(f"Ищем машины в диапазоне от {min_price} до {max_price} рублей.")
+
+        # Вывод в терминал
+        print(f"Минимальная цена: {min_price}, Максимальная цена: {max_price}")
+
+        # Завершаем состояние
+        await state.clear()
+    except ValueError:
+        await message.answer("Пожалуйста, введите данные в правильном формате: 100000 200000")
 
 
 # Асинхронный запуск бота
 async def main():
     print("Бот запущен!")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
